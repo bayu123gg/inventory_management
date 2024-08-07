f9d9153effa950be0d9afff036673f26f5b9ce74
ubeann <76271600+ubeann@users.noreply.github.com>
2024-08-08 02:24:51 +0700

    feat: create navigation bar

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:inventory_management/components/bar/navigation.dart';
 import 'package:inventory_management/components/bar/title.dart';
 import 'package:inventory_management/modules/home/home_controller.dart';
 import 'package:inventory_management/utils/utils.dart';
@@ -7,46 +8,17 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: const TitleBar(
+        appBar: TitleBar(
           title: Application.name,
           leadingImageAsset: 'lib/assets/icons/icon_box.svg',
           isLeadingSvg: true,
         ),
-        body: Stack(
-          children: <Widget>[
-            // Widget utama
-
-            // Widget box hitam di bagian dasar layar
-            Positioned(
-              left: 0.0,
-              bottom: 0.0,
-              right: 0,
-              child: Container(
-                height: 53,
-                color: const Color.fromARGB(255, 52, 51, 51),
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 15.0,
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.black,
-                        ),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        body: Center(
+          child: Text('Main content goes here'),
         ),
+        bottomNavigationBar: NavBar(),
       ),
     );
   }
